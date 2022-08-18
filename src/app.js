e08d7f9052a8e00a8b54672c7af4d6243534f971
ElverGM <unidroit.lex@gmail.com>
2022-08-18 11:18:41 +0000

    ready to upload

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  function displayNumbers() {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+    return singleNumber;
+  }
+  function displaySuits() {
+    let suits = ["♥", "♣", "♦", "♠"];
+    let singlesuits = suits[Math.floor(Math.random() * suits.length)];
+    return singlesuits;
+  }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let numbersCard = displayNumbers();
+  let suitsCard = displaySuits();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let spanNumber = document.querySelector(".number");
+  spanNumber.innerHTML = numbersCard;
+  let spanTopSuit = document.querySelector(".top-suit");
+  spanTopSuit.innerHTML = suitsCard;
+  let spanBottomSuit = document.querySelector(".bottom-suit");
+  spanBottomSuit.innerHTML = suitsCard;
+
+  if (suitsCard == "♥" || suitsCard == "♦") {
+    spanNumber.style.color = "red";
+    spanTopSuit.style.color = "red";
+    spanBottomSuit.style.color = "red";
+  }
 };
